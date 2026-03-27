314e3497b7b8cfcc58f89c502d3429a548e9fa4e
Charles B <bicari.charles@gmail.com>
2026-03-27 19:00:30 -0400

    modded OSC message structure to facilitate manipulation in intermedia software, brought magnetometer readings back to Gauss instead of uTesla

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -214,33 +214,28 @@ void loop() {
     OSCBundle bundle;
     osctime_t timetag;
 
-    bundle.add((osc_prefix + "/IMU").c_str())
-        .add(puaraIMU.accl.x)
-        .add(puaraIMU.accl.y)
-        .add(puaraIMU.accl.z);
+    bundle.add((osc_prefix + "/IMU/accl/x").c_str()).add(puaraIMU.accl.x);
+    bundle.add((osc_prefix + "/IMU/accl/y").c_str()).add(puaraIMU.accl.y);
+    bundle.add((osc_prefix + "/IMU/accl/z").c_str()).add(puaraIMU.accl.z);
 
-    bundle.add((osc_prefix + "/gyro").c_str())
-        .add(puaraIMU.gyro.x)
-        .add(puaraIMU.gyro.y)
-        .add(puaraIMU.gyro.z);
+    bundle.add((osc_prefix + "/IMU/gyro/x").c_str()).add(puaraIMU.gyro.x);
+    bundle.add((osc_prefix + "/IMU/gyro/y").c_str()).add(puaraIMU.gyro.y);
+    bundle.add((osc_prefix + "/IMU/gyro/z").c_str()).add(puaraIMU.gyro.z);
 
-    bundle.add((osc_prefix + "/magn").c_str())
-        .add(puaraIMU.magn.x)
-        .add(puaraIMU.magn.y)
-        .add(puaraIMU.magn.z);
+    bundle.add((osc_prefix + "/IMU/magn/x").c_str()).add(puaraIMU.magn.x);
+    bundle.add((osc_prefix + "/IMU/magn/y").c_str()).add(puaraIMU.magn.y);
+    bundle.add((osc_prefix + "/IMU/magn/z").c_str()).add(puaraIMU.magn.z);
 
-    bundle.add((osc_prefix + "/quat").c_str())
-        .add(puaraQuat.w)
-        .add(puaraQuat.x)
-        .add(puaraQuat.y)
-        .add(puaraQuat.z);
+    bundle.add((osc_prefix + "/IMU/quat/w").c_str()).add(puaraQuat.w);
+    bundle.add((osc_prefix + "/IMU/quat/x").c_str()).add(puaraQuat.x);
+    bundle.add((osc_prefix + "/IMU/quat/y").c_str()).add(puaraQuat.y);
+    bundle.add((osc_prefix + "/IMU/quat/z").c_str()).add(puaraQuat.z);
 
-    bundle.add((osc_prefix + "/YPR").c_str())
-        .add(puaraYPR.x)
-        .add(puaraYPR.y)
-        .add(puaraYPR.z);
+    bundle.add((osc_prefix + "/IMU/YPR/Roll").c_str()).add(puaraYPR.x);
+    bundle.add((osc_prefix + "/IMU/YPR/Pitch").c_str()).add(puaraYPR.y);
+    bundle.add((osc_prefix + "/IMU/YPR/Yaw").c_str()).add(puaraYPR.z);
 
-    bundle.add((osc_prefix + "/ultrasonic").c_str())
+    bundle.add((osc_prefix + "/ultrasonic/distance").c_str())
         .add(static_cast<int32_t>(sensors.ultDistance));
 
     bundle.add((osc_prefix + "/touch").c_str()).add(touch.getValue());
@@ -256,17 +251,15 @@ void loop() {
         .add(button.tripleTap);
     bundle.add((osc_prefix + "/button/count").c_str()).add(button.count);
 
-    bundle.add(("/" + puara.dmi_name() + "/jab").c_str())
-        .add(jab.x.current_value())
-        .add(jab.y.current_value())
-        .add(jab.z.current_value());
+    bundle.add(("/" + puara.dmi_name() + "/jab/x").c_str()).add(jab.x.current_value());
+    bundle.add(("/" + puara.dmi_name() + "/jab/y").c_str()).add(jab.y.current_value());
+  bundle.add(("/" + puara.dmi_name() + "/jab/z").c_str()).add(jab.z.current_value());
 
-    bundle.add(("/" + puara.dmi_name() + "/shake").c_str())
-        .add(shake.x.current_value())
-        .add(shake.y.current_value())
-        .add(shake.z.current_value());
+    bundle.add(("/" + puara.dmi_name() + "/shake/x").c_str()).add(shake.x.current_value());
+    bundle.add(("/" + puara.dmi_name() + "/shake/y").c_str()).add(shake.y.current_value());
+    bundle.add(("/" + puara.dmi_name() + "/shake/z").c_str()).add(shake.z.current_value());
 
-    bundle.add(("/" + puara.dmi_name() + "/battery").c_str())
+    bundle.add(("/" + puara.dmi_name() + "/battery/percentage").c_str())
         .add(battery.percentage);
 
     Udp.beginPacket(oscIP.c_str(), oscPort);
